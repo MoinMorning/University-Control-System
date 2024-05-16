b6fddcd506d14b7ad03c7c319541ca2c780b356b
Moin Moin <79722687+MoinMorning@users.noreply.github.com>
2024-05-17 01:15:52 +0200

    Create project Spring Boot for Univeristy Admin-System

---
+++ src/main/java/com/example/uni/student/Student.java
@@ -0,0 +1,11 @@
+package com.example.uni.student;
+
+import java.time.LocalDate;
+
+public class Student {
+    private int id;
+    private String name;
+    private String campusMail;
+    private LocalDate date;
+    private int age;
+}
