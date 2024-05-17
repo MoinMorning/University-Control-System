70a67685487dae3ab8a0e8a44f7d2ddcb3e3375f
Moin Moin <79722687+MoinMorning@users.noreply.github.com>
2024-05-17 21:10:11 +0200

    Postmapping to addnewstudent

--- src/main/java/com/example/uni/student/StudentController.java
+++ src/main/java/com/example/uni/student/StudentController.java
@@ -2,6 +2,7 @@ package com.example.uni.student;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +20,10 @@ public class StudentController {
     @GetMapping
     public List<Student> getStudents(){
         return studentService.getStudents();
+    }
+    @PostMapping
+    public void registerNewStudent(Student student){
+        studentService.addNewStudent(student);
+
     }
 }
